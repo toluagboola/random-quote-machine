a6a5db2a9afca0b0dbed492ed7f85d3dd4aca172
Toluwanimi Agboola <toluwanimiagboola@gmail.com>
2020-10-14 12:55:44 +0100

    Initial commit

---
+++ main.js
@@ -0,0 +1,38 @@
+const twitterButton = document.querySelector('#js-tweet');
+const spinner = document.querySelector('#js-spinner');
+const newQuoteButton = document.querySelector('#js-new-quote');
+newQuoteButton.addEventListener('click', getQuote);
+
+const endpoint = 'https://api.quotable.io/random';
+
+async function getQuote() {
+  spinner.classList.remove('hidden');
+  newQuoteButton.disabled = true;
+
+  try {
+    const response = await fetch(endpoint); 
+    if(!response.ok) {
+      throw Error(response.statusText)
+    }
+    const json = await response.json();
+    displayQuote(json.content + " - " + json.author);
+    setTweetButton(json.content, json.author);
+  } catch(err) {
+    console.log(err);
+    alert('Failed to fetch new quote');
+  } finally {
+    newQuoteButton.disabled = false;
+    spinner.classList.add('hidden');
+  }
+}
+
+function displayQuote(quote) {
+  const quoteText = document.querySelector('#js-quote-text');
+  quoteText.textContent = quote;
+}
+
+function setTweetButton(quote, author) {
+  twitterButton.setAttribute('href', `https://twitter.com/share?text=${quote} - ${author}`);
+}
+
+getQuote();
